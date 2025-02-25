183197a8fa5341befe76f3f0e2e1729af61e990d
Anna <anna.zadoiko@gmail.com>
2025-02-25 09:21:59 +0200

    qq

--- src/main/java/com/example/constance/museum/MuseumEnum.java
+++ src/main/java/com/example/constance/museum/MuseumEnum.java
@@ -3,7 +3,7 @@ package com.example.constance.museum;
 public enum MuseumEnum {
     INFO ("Інформація про музей"),
     SOCIAL_MEDIA_MUSEUM ("Соціальні мережі"),
-    REGISTRATION ("Реєстрація");
+    REGISTRATION ("Реєстрація на екскурсію");
 
     private String fullName;
 
